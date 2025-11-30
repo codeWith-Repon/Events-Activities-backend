aeb680a9ede734779a75a9046d761ef1088affdf
Md Repon <repon7253@gmail.com>
2025-11-30 23:15:53 +0600

    🛡️ 🛡️ route gard for get user route.
    Add cookie-parser middleware and implement JWT verification in user routes

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,12 @@ import cors from 'cors'
 import { globalErrorHandler } from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
 import { router } from './app/routes';
-
+import cookieParser from 'cookie-parser'
 const app: Application = express();
 
 
 app.use(cors())
+app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,8 +1,11 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { UserController } from "./user.controller";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { createUserSchema, updateUserSchema } from "./user.validation";
 import { multerUpload } from "../../config/multer.config";
+import jwt, { JwtPayload } from "jsonwebtoken"
+import { envVars } from "../../config/env";
+import { UserRole } from "../../../generated/prisma/enums";
 
 const router = Router()
 
@@ -14,6 +17,25 @@ router.post(
 
 router.get(
     "/",
+    async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const accessToken = req.cookies.accessToken
+
+            if (!accessToken) {
+                throw new Error("No access token found")
+            }
+
+            const verifiedToken = jwt.verify(accessToken, envVars.JWT_ACCESS_SECRET)
+
+            if ((verifiedToken as JwtPayload).role !== UserRole.USER) {
+                throw new Error("Unauthorized")
+            }
+            console.log(verifiedToken)
+            next()
+        } catch (error) {
+            next(error)
+        }
+    },
     UserController.getAllUsers
 );
 
