9ab83abea7209dcfe69e75c88082db466d6def9c
Md Repon <repon7253@gmail.com>
2025-12-01 01:56:55 +0600

    Implement password reset and change functionality; add token refresh endpoint

--- src/app/utils/userToken.ts
+++ src/app/utils/userToken.ts
@@ -1,6 +1,10 @@
-import { User } from "../../generated/prisma/client";
+import { JwtPayload } from "jsonwebtoken";
+import { Status, User } from "../../generated/prisma/client";
 import { envVars } from "../config/env";
-import { generateToken } from "./jwt";
+import { generateToken, verifyToken } from "./jwt";
+import { prisma } from "../../lib/prisma";
+import AppError from "../errorHelpers/AppError";
+import status from "http-status";
 
 
 export const createUserToken = (user: Partial<User>) => {
@@ -16,5 +20,41 @@ export const createUserToken = (user: Partial<User>) => {
     return {
         accessToken,
         refreshToken
+    }
+}
+
+export const createNewAccessTokenWitRefreshToken = async (refreshToken: string) => {
+    const verifiedRefreshToken = verifyToken(refreshToken, envVars.JWT_REFRESH_SECRET) as JwtPayload
+
+    const isUserExist = await prisma.user.findUnique({
+        where: {
+            id: verifiedRefreshToken.userId
+        }
+    })
+
+    if (!isUserExist) {
+        throw new AppError(status.BAD_REQUEST, "User doesn't exist")
+
+    }
+
+    if (isUserExist.status === Status.INACTIVE || isUserExist.status === Status.BLOCKED) {
+        throw new AppError(status.FORBIDDEN, `User is ${isUserExist.status}`)
+    }
+
+    if (isUserExist.isDeleted) {
+        throw new AppError(status.FORBIDDEN, "User is deleted")
+    }
+
+    const jwtPayload = {
+        userId: isUserExist.id,
+        email: isUserExist.email,
+        role: isUserExist.role
+    }
+    const generateAccessToken = generateToken(jwtPayload, envVars.JWT_ACCESS_SECRET, envVars.JWT_ACCESS_EXPIRES);
+    const generateRefreshToken = generateToken(jwtPayload, envVars.JWT_REFRESH_SECRET, envVars.JWT_REFRESH_EXPIRES);
+
+    return {
+        accessToken: generateAccessToken,
+        refreshToken: generateRefreshToken
     }
 }
